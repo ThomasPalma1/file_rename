fb584fa9bc347329779f1021a0b4056450a2c9e2
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-07-30 16:56:10 -0300

    refactor: improving script structure

--- src/extension_based_directory_organizer.py
+++ src/extension_based_directory_organizer.py
@@ -1,15 +1,22 @@
 import os
 import shutil
 
-path = input("Enter the directory you want to organize:\n")
-files = os.listdir(path)
+def organize_files(path):
+    files = os.listdir(path)
 
-for file in files:
-    filename, extension = os.path.splitext(file)
-    extension = extension[1:]
+    for file in files:
+        filename, extension = os.path.splitext(file)
+        extension = extension[1:]
 
-    if os.path.exists(path+'/'+extension):
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
-    else:
-        os.makedirs(path+'/'+extension)
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+        if os.path.exists(os.path.join(path, extension)):
+            try:
+                shutil.move(os.path.join(path, file), os.path.join(path, extension, file))
+            except PermissionError:
+                print(f"Cannot move '{file}' as it's in use by another process.")
+        else:
+            os.makedirs(os.path.join(path, extension))
+            shutil.move(os.path.join(path, file), os.path.join(path, extension, file))
+
+if __name__ == "__main__":
+    path = input("Enter the directory you want to organize:\n")
+    organize_files(path)
