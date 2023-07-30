870a8effc127feac6d5a09c045a67bc09d4471f3
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-07-30 18:54:09 -0300

    refactor: changing variable name

--- src/extension_based_directory_organizer.py
+++ src/extension_based_directory_organizer.py
@@ -1,22 +1,23 @@
 import os
 import shutil
+from loguru import logger
 
 
-def organize_files(path):
-    files = os.listdir(path)
+def organize_files(directory):
+    files = os.listdir(directory)
 
     for file in files:
         filename, extension = os.path.splitext(file)
         extension = extension[1:]
 
-        if os.path.exists(os.path.join(path, extension)):
+        if os.path.exists(os.path.join(directory, extension)):
             try:
-                shutil.move(os.path.join(path, file), os.path.join(path, extension, file))
+                shutil.move(os.path.join(directory, file), os.path.join(directory, extension, file))
             except PermissionError:
-                print(f"Cannot move '{file}' as it's in use by another process.")
+                logger.info(f"Cannot move '{file}' as it's in use by another process.")
         else:
-            os.makedirs(os.path.join(path, extension))
-            shutil.move(os.path.join(path, file), os.path.join(path, extension, file))
+            os.makedirs(os.path.join(directory, extension))
+            shutil.move(os.path.join(directory, file), os.path.join(directory, extension, file))
 
 
 if __name__ == "__main__":
